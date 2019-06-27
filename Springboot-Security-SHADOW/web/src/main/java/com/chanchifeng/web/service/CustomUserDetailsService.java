cdb4261c138d2c1669c7d0bc41e7cbdbc06d2b23
porschan <710437653@qq.com>
2019-06-27 12:00:26 +0800

    add security

---
+++ Springboot-Security-SHADOW/web/src/main/java/com/chanchifeng/web/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.chanchifeng.web.service;
+
+import com.chanchifeng.mysql.entity.User;
+import com.chanchifeng.mysql.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepository.findByName(userName);
+        if (user == null) {
+            throw new UsernameNotFoundException("UserName " + userName + " not found");
+        }
+        return new SecurityUser(user);
+    }
+}
